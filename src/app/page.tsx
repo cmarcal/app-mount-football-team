738dbbae53e5566b8f699bb81d56243bb68952e9
Roberto Marçal <cmarcal.roberto@gmail.com>
2023-06-03 18:04:58 -0300

    chore: update images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 
   return (
     <main className="bg-[url('../img/home.jpeg')] bg-no-repeat bg-cover flex h-full flex-col justify-center items-center">
-      <div className="flex flex-col items-center justify-between text-white h-4/5">
+      <div className="flex flex-col items-center justify-around text-white h-3/5">
         <h1 className="text-5xl font-semibold tracking-tighter text-center">
           HOJE É DIA <br />
           <span className="text-yellow-500 text-7xl">DE JOGO!</span>
