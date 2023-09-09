0f07b7ce157804b0c96388568df820301612e4f0
Roberto Marçal <cmarcal.roberto@gmail.com>
2023-09-09 11:14:20 -0300

    fix: add trim on input

--- src/app/campo/[id]/components/memberQueue/MemberQueue.tsx
+++ src/app/campo/[id]/components/memberQueue/MemberQueue.tsx
@@ -25,7 +25,7 @@ export const MemberQueue = ({ addPlayer } : MemberQueueParams ) => {
     <div className="flex flex-col w-full gap-4 ">
       <form className="flex flex-col gap-3 p-3 border-2 border-yellow-500 border-solid rounded-md w-100" onSubmit={submitForm}>
         <div className="flex gap-4 w-100">
-          <input value={playerName} onChange={({target}) => setPlayerName(target.value)} className="w-full px-2 py-1 border-2 rounded-md" placeholder="Nome do jogador"/>
+          <input value={playerName} onChange={({target}) => setPlayerName(target.value.trim())} className="w-full px-2 py-1 border-2 rounded-md" placeholder="Nome do jogador"/>
           <div className="flex items-center justify-center gap-2 p-2 rounded-md bg-slate-50 ">
             <input checked={isMonthlyWorker} onChange={({target}) => setIsMonthlyWorker(target.checked)} type="checkbox" name="mensalista" id="mensalista" className="cursor-pointer"/>
             <label htmlFor="mensalista" className="text-sm cursor-pointer">mensalista</label>
